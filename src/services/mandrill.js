b45ececbf977f3890c4ed2783a776ed4bf20da19
mmazzarolo <mazzarolomatteo@gmail.com>
2017-05-25 10:33:43 +0200

    Testing stage-lint

--- src/services/mandrill.js
+++ src/services/mandrill.js
@@ -34,7 +34,7 @@ const sendVerifyAccountEmail = async (to, url) => {
   const text = `
     You are receiving this email because you (or someone else) have requested the creation of a new account.\n\n
     Please click on the following link, or paste it into your browser to complete the process:\n
-    ${url} 
+    ${url}\n
     If you did not request this, please ignore this email.\n
   `;
   await sendEmail(to, 'Confirm your account', text);
@@ -44,7 +44,7 @@ const sendPasswordResetEmail = async (to, url) => {
   const text = `
     You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
     Please click on the following link, or paste it into your browser to complete the process:\n
-    ${url}
+    ${url}\n
     If you did not request this, please ignore this email and your password will remain unchanged.\n
   `;
   await sendEmail(to, 'Password reset', text);
