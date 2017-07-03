7229f8c2ee77882d954d40f123dc4347960a3a78
mmazzarolo <mazzarolomatteo@gmail.com>
2017-07-03 11:02:41 +0200

    Better error handling

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,25 +1,24 @@
 const koaBouncer = require('koa-bouncer');
+const http = require('http');
 const isNumber = require('lodash').isNumber;
 
 module.exports = async (ctx, next) => {
   try {
     await next();
+    if (ctx.response.status === 404 && !ctx.response.body) ctx.throw(404);
   } catch (err) {
+    let status;
+    if (err instanceof koaBouncer.ValidationError) {
+      status = 422;
+    } else {
+      status = isNumber(err.status) ? err.status : 500;
+    }
     ctx.type = 'application/json';
     ctx.body = {};
-    if (err instanceof koaBouncer.ValidationError) {
-      ctx.status = 422;
-      ctx.body.error = {
-        message: err.message || 'Validation error',
-        status: 422,
-      };
-    } else {
-      const status = isNumber(err.status) ? err.status : 500;
-      ctx.status = status;
-      ctx.body.error = {
-        message: err.message || 'Internal server error',
-        status: status,
-      };
-    }
+    ctx.status = status;
+    ctx.body.error = {
+      message: err.message || http.STATUS_CODES[status] || 'Internal server error',
+      status: status,
+    };
   }
 };
