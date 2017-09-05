5845fe3b30d31dc93066f3b3d6f741267a687245
mmazzarolo <mazzarolomatteo@gmail.com>
2017-09-05 09:00:29 +0200

    Updated dependencies

--- src/db/seeds/index.js
+++ src/db/seeds/index.js
@@ -1,19 +1,20 @@
+const uuid = require('uuid');
 const cryptoUtils = require('../../utils/crypto');
 const knex = require('../connection');
 
 exports.userPlainTextPassword = 'johnson123';
 exports.user = {
-  id: '932fb35f-623d-44bd-b180-77a71eca5054',
+  id: uuid.v4(),
   email: 'jeremy@test.com',
   password: cryptoUtils.hashPassword(exports.userPlainTextPassword),
   emailVerified: true,
-  verifyEmailToken: 'n283r928j2029jjjflsakdfjasdf03',
-  resetPasswordToken: 'jid32soidji201s0n019rdju010dj10',
+  verifyEmailToken: cryptoUtils.createVerifyAccountToken(),
+  resetPasswordToken: cryptoUtils.createResetPasswordToken(),
   resetPasswordTokenExpiresAt: knex.raw(`NOW() + INTERVAL '1 hour'`),
 };
 
 exports.session = {
-  id: 'd4c418c9-274b-4a17-9817-fbc14c3e2017',
+  id: uuid.v4(),
   token: 'bdade1fd638e8bb4e252bbe8d17c2d63',
   userId: exports.user.id,
   ipAddress: '::ffff:127.0.0.1',
@@ -21,7 +22,7 @@ exports.session = {
 };
 
 exports.message = {
-  id: 'd4c418c9-274b-4a17-9817-fbc14c3e2017',
+  id: uuid.v4(),
   userId: exports.user.id,
   text: 'Hello world',
 };
